aa5095910280434dbc84b09a4a79183906a2df0b
tohird03 <tohirjondoniyorov@gmail.com>
2025-04-02 12:04:12 +0500

    Add sale price

--- src/pages/Products/ProductsList/ProductsList.tsx
+++ src/pages/Products/ProductsList/ProductsList.tsx
@@ -118,7 +118,7 @@ export const ProductsList = observer(() => {
                 <div style={{ textAlign: 'center', fontWeight: 'bold', maxWidth: '150px', margin: '0 auto' }}>
                   Umumiy qiymati:
                   <p style={{ margin: '0', fontWeight: 'bold' }}>
-                    {priceFormat(productsData?.data?.reduce((cur, prev) => cur + prev?.selling_price * prev?.count, 0))}
+                    {priceFormat(productsData?.data?.reduce((cur, prev) => cur + prev?.cost * prev?.count, 0))}
                   </p>
                 </div>
               </Table.Summary.Cell>
